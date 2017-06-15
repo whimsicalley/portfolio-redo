c0539b1550ee60fb46f252435ea2e7b11e53b089
Alley Manalio <amanalio@ufl.edu>
2017-06-14 22:07:30 -0400

    Adjust existing pages to reflect .editorconfig

--- 404.php
+++ 404.php
@@ -5,15 +5,15 @@ get_header(); ?>
 
   <div class="row">
     <div class="eight columns">
-      <h1>Not Found</h1>
-      <h2>This is somewhat embarrassing, isn't it?</h2>
-      <p>It looks like nothing was found at this location. Maybe try a search?</p>
+        <h1>Not Found</h1>
+        <h2>This is somewhat embarrassing, isn't it?</h2>
+        <p>It looks like nothing was found at this location. Maybe try a search?</p>
 
-      <h2>Try searching for what you need:</h2>
-      <?php get_search_form(); ?>
+        <h2>Try searching for what you need:</h2>
+        <?php get_search_form(); ?>
     </div>
     <div class="four columns">
-      <?php get_sidebar(); ?>
+        <?php get_sidebar(); ?>
     </div>
   </div>
 
