50b5451cd8fd7a79f8a3c8b18738f24cb423e575
Alley Manalio <amanalio@ufl.edu>
2017-06-22 13:34:04 -0400

    Create full width page and populate it

---
+++ page_fullwidth.php
@@ -0,0 +1,22 @@
+<?php
+/* Template Name: Full Width */
+?>
+
+<?php get_header(); ?>
+
+    <section class="row">
+        <div class="twelve columns">
+            <!-- Begin Loop -->
+            <?php if ( have_posts() ) {
+                while ( have_posts() ) {
+                    the_post(); ?>
+                    <h2><?php the_title(); ?></h2>
+                    <?php the_content();
+                }
+            }
+            ?>
+            <!-- End Loop -->
+        </div>
+    </section>
+
+<?php get_footer(); ?>
