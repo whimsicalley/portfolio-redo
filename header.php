b23c25f15a26430092b06efa50521da6063a8b5c
Alley Manalio <amanalio@ufl.edu>
2017-06-21 21:55:23 -0400

    Align main nav with logo and clean up CSS

--- header.php
+++ header.php
@@ -9,24 +9,24 @@
 <body>
     <div class="container">
         <header class="row">
-            <div class="eight columns" id="header-container">
+            <div class="two columns" id="header-container">
                 <div id="logo">
                     <a href="index.html"><img src="http://www.alleymanalio.com/work/wp-content/uploads/2017/06/logo_original.png" alt="Alley Manalio - Logo image" title="Alley Manalio - Logo Image" width="100" /></a>
                 </div>
+            </div>
+            <div class="six columns">
+                <!-- MENU CODE BEGINS -->
+                <div>
+                    <?php
+                        wp_nav_menu( array (
+                            'sort_column' => 'menu_order',
+                            'container_class' => 'main-menu-header'
+                      ));
+                    ?>
+                </div>
+            <!-- MENU CODE ENDS -->
             </div>
             <div class="four columns">
                 <h3>Search Bar</h3>
             </div>
         </header>
-        <!-- MENU CODE BEGINS -->
-        <div class="row">
-            <div class="twelve columns">
-            <?php
-                wp_nav_menu( array (
-                    'sort_column' => 'menu_order',
-                    'container_class' => 'blank-menu-header'
-              ));
-            ?>
-            </div>
-        </div>
-    <!-- MENU CODE ENDS -->
