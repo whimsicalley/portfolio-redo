0a48b499b87a446a6a190644a6de3adaf7022294
Alley Manalio <amanalio@ufl.edu>
2017-07-05 13:31:44 -0400

    Call to JS files in the footer

--- footer.php
+++ footer.php
@@ -8,6 +8,9 @@
         </footer>
         <!-- End Footer -->
     </div> <!-- End Container Div -->
+    <script src="js/jquery-3.1.0.min.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/main.js"></script>
     <?php wp_footer(); ?>
 </body>
 </html>
