0c8042769404a9440a52719ffcbb996f33a09d0f
Alley Manalio <amanalio@ufl.edu>
2017-07-07 14:00:56 -0400

    Create web design template page

---
+++ page_webdesign.php
@@ -0,0 +1,30 @@
+<?php
+/* Template Name: Full Width */
+?>
+
+<?php get_header(); ?>
+
+    <section class="row">
+        <div class="twelve columns">
+            <section>
+                <!-- Insert Image Slider here-->
+            </section>
+            <section>
+                <div class="breadcrumbs">
+                    <?php if ( function_exists( 'bcn_display' ) ) { bcn_display(); } ?>
+                </div>
+                <!-- Begin Loop -->
+                <?php if ( have_posts() ) {
+                    while ( have_posts() ) {
+                        the_post(); ?>
+                        <h2><?php the_title(); ?></h2>
+                        <?php the_content();
+                    }
+                }
+                ?>
+                <!-- End Loop -->
+            </section>
+        </div>
+    </section>
+
+<?php get_footer(); ?>
