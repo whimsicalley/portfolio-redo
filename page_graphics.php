9cc05e91ce2a2929557e67012293b48177e08c6f
Alley Manalio <amanalio@ufl.edu>
2017-07-14 20:05:06 -0400

    Overhaul graphics page; select specific category
    Selected specific category to show on the graphics page, in this case
    "5" which is "graphics."

--- page_graphics.php
+++ page_graphics.php
@@ -6,24 +6,29 @@
 
     <section class="row">
         <div class="twelve columns">
-            <section>
-                <!-- Insert Image Slider here-->
-            </section>
-            <section>
-                <div class="breadcrumbs">
-                    <?php if ( function_exists( 'bcn_display' ) ) { bcn_display(); } ?>
-                </div>
-                <!-- Begin Loop -->
-                <?php if ( have_posts() ) {
-                    while ( have_posts() ) {
-                        the_post(); ?>
-                        <h2><?php the_title(); ?></h2>
-                        <?php the_content();
-                    }
-                }
-                ?>
-                <!-- End Loop -->
-            </section>
+            <div class="breadcrumbs">
+                <?php if ( function_exists( 'bcn_display' ) ) { bcn_display(); } ?>
+            </div>
+            <h2>Graphics Examples</h2>
+            <p>Here you can find examples on some of my graphics work that I've done recently.</p>
+
+            <!-- Begin Loop for posts? Hopefully? -->
+            <?php query_posts('cat=5'); ?> <!--telling it which category to select & show-->
+            <?php
+                if ( have_posts() ) {
+                while ( have_posts() ) {
+                    the_post(); ?>
+                    <a href="<?php the_permalink(); ?>">
+                    <?php
+                        if ( has_post_thumbnail() ) {
+                        the_post_thumbnail('thumbnail');
+                        }
+                    ?></a>
+                <?php
+                } //end while
+                } //end if
+            ?>
+            <!-- End Loop -->
         </div>
     </section>
 
